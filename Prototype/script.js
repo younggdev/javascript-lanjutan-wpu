ca682a1b42eb696e575e4fbaf8b6321d9bf4a578
YoungDevWeb <youngdev50@gmail.com>
2023-06-09 09:36:12 -0700

    first commit

---
+++ Prototype/script.js
@@ -0,0 +1,65 @@
+// 1. Object Literal
+let mahasiswa1 = {
+  nama: "Ahmad",
+  energi: 10,
+  makan: function (porsi) {
+    this.energi = this.energi + porsi;
+    console.log(`Halo ${this.nama}, selamat makan`);
+  },
+};
+
+let mahasiswa2 = {
+  nama: "Maulana",
+  energi: 20,
+  makan: function (porsi) {
+    this.energi = this.energi + porsi;
+    console.log(`Halo ${this.nama}, selamat makan`);
+  },
+};
+
+// 2. Function Declaration
+const methodMahasiswa = {
+  makan: function (porsi) {
+    this.energi += porsi;
+    console.log(`Halo ${this.nama}, selamat makan `);
+  },
+
+  main: function (jam) {
+    this.energi -= jam;
+    console.log(`Halo ${this.nama}, selamat main`);
+  },
+};
+
+function Mahasiswa(nama, energi) {
+  let mahasiswa = {};
+  mahasiswa.nama = nama;
+  mahasiswa.energi = energi;
+  mahasiswa.makan = methodMahasiswa.makan;
+  mahasiswa.main = methodMahasiswa.main;
+
+  return mahasiswa;
+}
+
+let Ahmad = Mahasiswa("Ahmad", 10);
+let Maulana = Mahasiswa("Maulana", 20);
+
+// 3. Constructor Function
+// keyword new
+function Mahasiswa1(nama, energi) {
+  this.nama = nama;
+  this.energi = energi;
+
+  this.makan = function (porsi) {
+    this.energi += porsi;
+    console.log(`Halo ${this.nama}, selamat makan `);
+  };
+
+  this.main = function (jam) {
+    this.energi -= jam;
+    console.log(`Halo ${this.nama}, selamat main`);
+  };
+}
+
+let Ahmad1 = new Mahasiswa1("Ahmad", 10);
+
+// 4.
